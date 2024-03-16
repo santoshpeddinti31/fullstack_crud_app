8153a875bda282d5a7d7a2e8ec14ea80293bc33c
santosh peddinti <santoshpeddinti1997@gmail.com>
2024-03-16 14:09:02 +0530

    spring data rest

--- src/main/java/com/example/fullstackcrudapp/demo/dao/TodoRepository.java
+++ src/main/java/com/example/fullstackcrudapp/demo/dao/TodoRepository.java
@@ -2,6 +2,8 @@ package com.example.fullstackcrudapp.demo.dao;
 
 import com.example.fullstackcrudapp.demo.entity.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "data")
 public interface TodoRepository extends JpaRepository<Todo,Integer> {
 }
