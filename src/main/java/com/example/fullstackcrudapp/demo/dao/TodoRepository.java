5059be6c2c4b4135b19c1ff692f12a14be09ad00
santosh peddinti <santoshpeddinti1997@gmail.com>
2024-03-19 11:48:52 +0530

    backend springboot

--- src/main/java/com/example/fullstackcrudapp/demo/dao/TodoRepository.java
+++ src/main/java/com/example/fullstackcrudapp/demo/dao/TodoRepository.java
@@ -3,7 +3,7 @@ package com.example.fullstackcrudapp.demo.dao;
 import com.example.fullstackcrudapp.demo.entity.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-@RepositoryRestResource(path = "data")
 public interface TodoRepository extends JpaRepository<Todo,Integer> {
 }
---
+++ src/main/java/com/example/fullstackcrudapp/demo/rest/TodoRestController.java
@@ -0,0 +1,59 @@
+package com.example.fullstackcrudapp.demo.rest;
+
+import com.example.fullstackcrudapp.demo.entity.Todo;
+import com.example.fullstackcrudapp.demo.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin("*")
+public class TodoRestController {
+
+    private TodoService todoService;
+
+    @Autowired
+    public TodoRestController(TodoService theTodoService)
+    {
+        this.todoService=theTodoService;
+    }
+
+    @GetMapping("/todos")
+    public List<Todo> findAll()
+    {
+        return todoService.findAll();
+    }
+
+    @GetMapping("/todos/{todoId}")
+    public Todo getTodo(@PathVariable int todoId)
+    {
+        Todo theTodo=todoService.findById(todoId);
+
+        return theTodo;
+    }
+
+    @PostMapping("/todos")
+    public Todo save(@RequestBody Todo theTodo)
+    {
+        Todo dbTodo=todoService.save(theTodo);
+        return dbTodo;
+    }
+
+    @PutMapping("/todos")
+    public Todo updateTodo(@RequestBody Todo theTodo)
+    {
+        Todo dbTodo=todoService.save(theTodo);
+
+        return dbTodo;
+    }
+
+    @DeleteMapping("/todos/{theId}")
+    public String deleteTodoId(@PathVariable int theId)
+    {
+        Todo theTodo=todoService.findById(theId);
+        todoService.deleteById(theTodo.getId());
+        return "Todo id is deleted -"+theId;
+    }
+}
